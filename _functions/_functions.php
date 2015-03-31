db5be35e8d196e219dd1f9b191d8116106784cde
Sebastian Kowalski <sebi@kowalski-voerde.de>
2015-03-31 03:12:16 +0200

    Layout user management

--- _functions/_functions.php
+++ _functions/_functions.php
@@ -79,4 +79,47 @@ function smar_post_status($status) {
 			break;
 	}
 }
+
+// parse role status
+function smar_parse_role_web($role_web) {
+	switch($role_web) {
+		case '0':
+			return "No web rights";
+			break;
+		case '1':
+			return "Read only";
+			break;
+		case '2':
+			return "Products & Units";
+			break;
+		case '3':
+			return "Products, Units, Shelves, Sections";
+			break;
+		case '4':
+			return "Edit all";
+			break;
+		case '8':
+			return "Manager";
+			break;
+		case '9':
+			return "Administrator";
+			break;
+		default:
+			return "Unknown - Code: ".$role_web;
+	}
+}
+
+// parse role status
+function smar_parse_role_device($role_web) {
+	switch($role_web) {
+		case '0':
+			return '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#FF0000" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>';
+			break;
+		case '1':
+			return '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#00FF00" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg>';
+			break;
+		default:
+			return '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#FF0000" d="M10,19H13V22H10V19M12,2A6,6 0 0,1 18,8C17.67,9.33 17.33,10.67 16.5,11.67C15.67,12.67 14.33,13.33 13.67,14.17C13,15 13,16 13,17H10C10,15.33 10,13.92 10.67,12.92C11.33,11.92 12.67,11.33 13.5,10.67C14.33,10 14.67,9 15,8A3,3 0 0,0 12,5A3,3 0 0,0 9,8H6A6,6 0 0,1 12,2Z" /></svg>';
+	}
+}
 ?>
