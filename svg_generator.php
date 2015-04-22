123a4ac57be5bf3b84157416cdeb282cf7d96264
pianomister <stegiemail-github@yahoo.de>
2015-04-22 11:57:39 +0200

    SVG generator: display section ID and name

--- svg_generator.php
+++ svg_generator.php
@@ -4,23 +4,26 @@ require_once('_functions/_functions.php');
 $tplSVG = '
 <?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg width="{{shelfX}}" height="{{shelfY}}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg width="{{shelfX}}" height="{{shelfY}}" viewBox="0 0 {{shelfX}} {{shelfY}}" style="width:100%;height: auto;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 	<title>Shelf \'{{shelfName}}\' (ID: {{shelfID}}, last updated: \'{{time}}\')</title>
 	
 	<defs>
 		<style type="text/css">
 		<![CDATA[
+		text {fill: #333;font-family:Roboto;font-size: 8px;}
 		rect {fill:#ccc; stroke:#777; stroke-width: 2px;}
-		.section {fill:#16a082; stroke:#555; stroke-width: 2px; opacity:.8;}
+		.section {fill:#ddd; stroke:#555; stroke-width: 1px; opacity:.8;}
+		.selected {fill:#16a082;}
 		]]>
 		</style>
 	</defs>
 	
-	<rect id="shelf{{shelfID}}" x="0" y="0" width="{{shelfX}}" height="{{shelfY}}" stroke-width="2" />
+	<rect id="shelf{{shelfID}}" x="0" y="0" width="{{shelfX}}" height="{{shelfY}}" />
 	{{sections}}
 </svg>
 ';
-$tplSection = '<rect id="section{{sectionID}}" x="{{sectionPosX}}" y="{{sectionPosY}}" width="{{sectionX}}" height="{{sectionY}}" class="section" />\n';
+$tplSection = '<rect id="section{{sectionID}}" x="{{sectionPosX}}" y="{{sectionPosY}}" width="{{sectionX}}" height="{{sectionY}}" class="section" />\n
+							 <text x="{{sectionTextX}}" y="{{sectionTextY}}">{{sectionID}}: {{sectionName}}</text>';
 
 // check if id for shelf is given
 if(isset($_GET['id'])) {
@@ -49,11 +52,27 @@ if(isset($_GET['id'])) {
 		
 		if($result_sections->num_rows > 0) {	
 			
-			$tmp_search = array('{{sectionID}}','{{sectionPosX}}','{{sectionPosY}}','{{sectionX}}','{{sectionY}}');
+			$tmp_search = array('{{sectionID}}',
+													'{{sectionPosX}}',
+													'{{sectionPosY}}',
+													'{{sectionX}}',
+													'{{sectionY}}',
+													'{{sectionName}}',
+													'{{sectionTextX}}',
+													'{{sectionTextY}}');
 			
 			while($section = $result_sections->fetch_array(MYSQLI_ASSOC)) {
 				$tmp = $tplSection;
-				$tmp_replace = array($section['section_id'], $section['position_x'], $section['position_y'], $section['size_x'], $section['size_y']);
+				
+				$tmp_replace = array($section['section_id'],
+														 $section['position_x'],
+														 $section['position_y'],
+														 $section['size_x'],
+														 $section['size_y'],
+														 $section['name'],
+														 $section['position_x']+5,
+														 $section['position_y']+15);
+				
 				$tmp = str_replace($tmp_search, $tmp_replace, $tmp);
 				$sections .= $tmp;
 			}
