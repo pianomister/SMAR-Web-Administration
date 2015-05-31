9540a92dc09f4d7c8e38adac528f1a9fc6520a96
pianomister <stegiemail-github@yahoo.de>
2015-05-31 23:12:05 +0200

    Add and update SQL dumps

--- smar_database_OLD_DEV.sql
+++ smar_database_OLD_DEV.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 31. Mai 2015 um 23:08
+-- Erstellungszeit: 02. Mrz 2015 um 15:43
 -- Server Version: 5.5.32
 -- PHP-Version: 5.4.19
 
@@ -30,13 +30,13 @@ USE `smar`;
 
 CREATE TABLE IF NOT EXISTS `smar_delivery` (
   `delivery_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `order_id` int(10) unsigned NOT NULL,
+	`order_id` int(10) unsigned NOT NULL,
   `date` datetime NOT NULL,
   `created` datetime NOT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`delivery_id`),
-  KEY `order_id` (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_delivery`:
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `smar_delivery` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `smar_delivery_item`
+-- Tabellenstruktur für Tabelle `smar_order_item`
 --
 
 CREATE TABLE IF NOT EXISTS `smar_delivery_item` (
@@ -66,12 +66,12 @@ CREATE TABLE IF NOT EXISTS `smar_delivery_item` (
 
 --
 -- RELATIONEN DER TABELLE `smar_delivery_item`:
+--   `delivery_id`
+--       `smar_delivery` -> `delivery_id`
 --   `unit_id`
 --       `smar_unit` -> `unit_id`
 --   `product_id`
 --       `smar_product` -> `product_id`
---   `delivery_id`
---       `smar_delivery` -> `delivery_id`
 --
 
 -- --------------------------------------------------------
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `smar_device` (
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`device_id`),
   UNIQUE KEY `hwaddress` (`hwaddress`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -131,10 +131,10 @@ CREATE TABLE IF NOT EXISTS `smar_map_shelf` (
 
 --
 -- RELATIONEN DER TABELLE `smar_map_shelf`:
---   `map_id`
---       `smar_map` -> `map_id`
 --   `shelf_id`
 --       `smar_shelf` -> `shelf_id`
+--   `map_id`
+--       `smar_map` -> `map_id`
 --
 
 -- --------------------------------------------------------
@@ -151,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `smar_order` (
   `created` datetime NOT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -171,16 +171,16 @@ CREATE TABLE IF NOT EXISTS `smar_order_item` (
   KEY `unit_id` (`unit_id`),
   KEY `product_id` (`product_id`),
   KEY `order_id` (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_order_item`:
+--   `order_id`
+--       `smar_order` -> `order_id`
 --   `unit_id`
 --       `smar_unit` -> `unit_id`
 --   `product_id`
 --       `smar_product` -> `product_id`
---   `order_id`
---       `smar_order` -> `order_id`
 --
 
 -- --------------------------------------------------------
@@ -195,15 +195,15 @@ CREATE TABLE IF NOT EXISTS `smar_product` (
   `barcode` bigint(15) unsigned DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `price` double NOT NULL DEFAULT '0',
-  `size_x` int(3) unsigned NOT NULL DEFAULT '0',
+	`size_x` int(3) unsigned NOT NULL DEFAULT '0',
   `size_y` int(3) NOT NULL DEFAULT '0',
   `size_z` int(3) NOT NULL DEFAULT '0',
-  `stackable` tinyint(1) unsigned NOT NULL DEFAULT '0',
+	`stackable` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `image` varchar(150) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -221,14 +221,14 @@ CREATE TABLE IF NOT EXISTS `smar_product_unit` (
   PRIMARY KEY (`product_unit_id`),
   KEY `unit_id` (`unit_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_product_unit`:
---   `unit_id`
---       `smar_unit` -> `unit_id`
 --   `product_id`
 --       `smar_product` -> `product_id`
+--   `unit_id`
+--       `smar_unit` -> `unit_id`
 --
 
 -- --------------------------------------------------------
@@ -243,7 +243,7 @@ CREATE TABLE IF NOT EXISTS `smar_section` (
   `product_id` int(10) unsigned NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `capacity` int(15) unsigned NOT NULL DEFAULT '0',
-  `min_capacity` int(15) unsigned NOT NULL DEFAULT '0',
+	`min_capacity` int(15) unsigned NOT NULL DEFAULT '0',
   `size_x` int(3) NOT NULL DEFAULT '0',
   `size_y` int(3) NOT NULL DEFAULT '0',
   `position_x` int(5) NOT NULL DEFAULT '0',
@@ -253,14 +253,14 @@ CREATE TABLE IF NOT EXISTS `smar_section` (
   PRIMARY KEY (`section_id`),
   KEY `shelf_id` (`shelf_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_section`:
---   `shelf_id`
---       `smar_shelf` -> `shelf_id`
 --   `product_id`
 --       `smar_product` -> `product_id`
+--   `shelf_id`
+--       `smar_shelf` -> `shelf_id`
 --
 
 -- --------------------------------------------------------
@@ -279,7 +279,7 @@ CREATE TABLE IF NOT EXISTS `smar_shelf` (
   `created` datetime NOT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`shelf_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -295,7 +295,7 @@ CREATE TABLE IF NOT EXISTS `smar_shelf_graphic` (
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`shelf_graphic_id`),
   KEY `shelf_id` (`shelf_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_shelf_graphic`:
@@ -318,7 +318,7 @@ CREATE TABLE IF NOT EXISTS `smar_stock` (
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`stock_id`),
   UNIQUE KEY `product_id` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- RELATIONEN DER TABELLE `smar_stock`:
@@ -339,7 +339,7 @@ CREATE TABLE IF NOT EXISTS `smar_unit` (
   `created` datetime NOT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`unit_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -369,7 +369,47 @@ CREATE TABLE IF NOT EXISTS `smar_user` (
 --
 
 INSERT INTO `smar_user` (`user_id`, `pnr`, `surname`, `lastname`, `username`, `role_web`, `role_device`, `password`, `salt`, `password_device`, `created`, `lastupdate`) VALUES
-(1, '1582885', '', 'Administrator', 'admin', 90, 1, '2323c469df9947e4e3525ea5e8cf701ee72775e374304282a9fb47074283a0b3', '0fd1d92468325d2350699fce039d2b342a5a435740fc13b0f0bbd4bbf1e2c2fe', '5627a3351231fb54c654ec49b85196bc9db01f4a8b71d3f4fb3fc33bdc584a52', '2015-03-02 12:13:42', '2015-05-29 08:24:15');
+(1, '123456', 'Admin', 'istrator', 'admin', 70, 1, '2323c469df9947e4e3525ea5e8cf701ee72775e374304282a9fb47074283a0b3', '0fd1d92468325d2350699fce039d2b342a5a435740fc13b0f0bbd4bbf1e2c2fe', NULL, '2015-03-02 12:13:42', '2015-05-01 16:35:48');
+
+--
+-- Beispieldaten TODO
+--
+INSERT INTO `smar_product` (`product_id`, `article_nr`, `barcode`, `name`, `price`, `size_x`, `size_y`, `size_z`, `stackable`, `image`, `created`, `lastupdate`) VALUES
+(1, 'ART953173', 953173, 'LekkaLekka Crunchy Bio-Chips', 2.99, 0, 0, 0, 0, 'NULL', '2015-04-03 00:16:12', '2015-04-02 22:16:12'),
+(2, 'ART473623', 584372615, 'MAMF Pampelmusencreme Brotaufstrich', 4.39, 0, 0, 0, 0, 'NULL', '2015-04-04 14:01:22', '2015-04-04 12:01:22'),
+(3, 'ART483721', 32847362, 'Förstermeister Kräuterlikör', 12.49, 12, 20, 4, 0, 'NULL', '2015-04-04 14:05:15', '2015-05-17 12:41:56'),
+(4, 'ART213211', 342761552, 'EKIA Garten-Klappstuhl', 21.99, 0, 0, 0, 0, 'NULL', '2015-04-04 14:06:53', '2015-04-17 12:17:32'),
+(5, 'ART230391', 2147483646, 'Vitalitasia Wasser still', 0.19, 0, 0, 0, 0, 'NULL', '2015-04-17 13:06:26', '2015-04-29 08:39:05'),
+(6, 'ART493872', 2147483647, 'Rotwein', 3.39, 8, 32, 8, 1, 'NULL', '2015-05-16 18:56:30', '2015-05-17 12:42:14'),
+(7, 'ART20000123', 2147483647, 'Holzuhr', 149, 5, 3, 10, 1, 'NULL', '2015-05-24 12:30:13', '2015-05-24 10:30:13'),
+(8, 'ART23123488', 238472376, 'SSD Festplatte 128GB', 69.9, 15, 4, 20, 1, 'NULL', '2015-05-24 12:32:56', '2015-05-24 10:32:56');
+INSERT INTO `smar_stock` (`stock_id`, `product_id`, `amount_warehouse`, `amount_shop`, `created`, `lastupdate`) VALUES
+(1, 7, 0, 0, '2015-05-24 12:30:13', '2015-05-24 10:30:13'),
+(2, 8, 2383, 23, '2015-05-24 12:32:56', '2015-05-24 10:32:56'),
+(3, 1, 123, 4576, '2015-03-02 12:13:42', '2015-05-24 10:34:14'),
+(4, 2, 423, 98, '2015-03-02 12:13:42', '2015-05-24 10:34:14'),
+(5, 3, 0, 0, '2015-03-02 12:12:00', '2015-05-24 10:34:32'),
+(6, 4, 0, 0, '2015-03-02 12:12:00', '2015-05-24 10:34:32'),
+(7, 5, 45, 32, NULL, '2015-05-24 10:34:51'),
+(8, 6, 153, 37, '2015-03-02 12:12:00', '2015-05-24 10:34:51');
+INSERT INTO `smar_unit` (`unit_id`, `name`, `capacity`, `created`, `lastupdate`) VALUES
+(1, 'Single', 1, '2015-04-17 14:51:33', '2015-04-17 12:52:56'),
+(2, 'Box 50', 50, '2015-04-17 14:59:38', '2015-04-17 12:59:38'),
+(3, 'Box 100', 100, '2015-04-17 14:59:46', '2015-04-17 12:59:46'),
+(4, 'Box 500', 500, '2015-04-17 15:01:08', '2015-04-17 13:11:30');
+INSERT INTO `smar_shelf` (`shelf_id`, `name`, `barcode`, `size_x`, `size_y`, `size_z`, `created`, `lastupdate`) VALUES
+(1, 'Süßwaren', 123324235, 1200, 200, 50, '2015-04-19 13:04:01', '2015-04-22 00:01:25'),
+(2, 'Softgetränke', 2147483647, 600, 180, 60, '2015-04-19 13:07:35', '2015-04-22 00:01:35'),
+(3, 'Weine', 34636234, 400, 200, 40, '2015-04-19 16:32:03', '2015-04-19 14:32:03');
+INSERT INTO `smar_section` (`section_id`, `shelf_id`, `product_id`, `name`, `capacity`, `size_x`, `size_y`, `position_x`, `position_y`, `created`, `lastupdate`) VALUES
+(1, 1, 3, 'Likör', 30, 50, 30, 30, 0, '2015-04-22 01:00:00', '2015-04-25 12:57:25'),
+(2, 1, 2, 'Aufstrich', 80, 80, 40, 200, 70, '2015-04-22 11:18:00', '2015-04-22 09:19:07'),
+(4, 2, 4, 'Test-Sektion', 120, 40, 40, 0, 0, '2015-04-25 14:43:25', '2015-04-25 17:14:33'),
+(5, 2, 3, 'Noch mehr Waldmeister', 20, 40, 60, 300, 0, '2015-04-25 19:05:25', '2015-04-25 17:05:25');
+
+
+
+
 
 --
 -- Constraints der exportierten Tabellen
@@ -385,38 +425,38 @@ ALTER TABLE `smar_delivery`
 -- Constraints der Tabelle `smar_delivery_item`
 --
 ALTER TABLE `smar_delivery_item`
+  ADD CONSTRAINT `smar_delivery_item_ibfk_3` FOREIGN KEY (`delivery_id`) REFERENCES `smar_delivery` (`delivery_id`),
   ADD CONSTRAINT `smar_delivery_item_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `smar_unit` (`unit_id`),
-  ADD CONSTRAINT `smar_delivery_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`),
-  ADD CONSTRAINT `smar_delivery_item_ibfk_3` FOREIGN KEY (`delivery_id`) REFERENCES `smar_delivery` (`delivery_id`);
+  ADD CONSTRAINT `smar_delivery_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`);
 
 --
 -- Constraints der Tabelle `smar_map_shelf`
 --
 ALTER TABLE `smar_map_shelf`
-  ADD CONSTRAINT `smar_map_shelf_ibfk_1` FOREIGN KEY (`map_id`) REFERENCES `smar_map` (`map_id`),
-  ADD CONSTRAINT `smar_map_shelf_ibfk_2` FOREIGN KEY (`shelf_id`) REFERENCES `smar_shelf` (`shelf_id`);
+  ADD CONSTRAINT `smar_map_shelf_ibfk_2` FOREIGN KEY (`shelf_id`) REFERENCES `smar_shelf` (`shelf_id`),
+  ADD CONSTRAINT `smar_map_shelf_ibfk_1` FOREIGN KEY (`map_id`) REFERENCES `smar_map` (`map_id`);
 
 --
 -- Constraints der Tabelle `smar_order_item`
 --
 ALTER TABLE `smar_order_item`
+  ADD CONSTRAINT `smar_order_item_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `smar_order` (`order_id`),
   ADD CONSTRAINT `smar_order_item_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `smar_unit` (`unit_id`),
-  ADD CONSTRAINT `smar_order_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`),
-  ADD CONSTRAINT `smar_order_item_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `smar_order` (`order_id`);
+  ADD CONSTRAINT `smar_order_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`);
 
 --
 -- Constraints der Tabelle `smar_product_unit`
 --
 ALTER TABLE `smar_product_unit`
-  ADD CONSTRAINT `smar_product_unit_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `smar_unit` (`unit_id`),
-  ADD CONSTRAINT `smar_product_unit_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`);
+  ADD CONSTRAINT `smar_product_unit_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`),
+  ADD CONSTRAINT `smar_product_unit_ibfk_1` FOREIGN KEY (`unit_id`) REFERENCES `smar_unit` (`unit_id`);
 
 --
 -- Constraints der Tabelle `smar_section`
 --
 ALTER TABLE `smar_section`
-  ADD CONSTRAINT `smar_section_ibfk_1` FOREIGN KEY (`shelf_id`) REFERENCES `smar_shelf` (`shelf_id`),
-  ADD CONSTRAINT `smar_section_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`);
+  ADD CONSTRAINT `smar_section_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `smar_product` (`product_id`),
+  ADD CONSTRAINT `smar_section_ibfk_1` FOREIGN KEY (`shelf_id`) REFERENCES `smar_shelf` (`shelf_id`);
 
 --
 -- Constraints der Tabelle `smar_shelf_graphic`
